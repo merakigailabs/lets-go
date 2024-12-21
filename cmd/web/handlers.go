3c19421e32a0c2e3500bc762bef41e97b547b1ba
Merakigai <merakigailabs@gmail.com>
2024-12-21 19:08:32 +0700

    ch4: add util to pass common properties to template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,17 +12,21 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	w.Header().Add("Server", "Go")
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
+	// Call the newTemplateData() helper to get a templateData struct containing
+	// the 'default' data (which for now is just the current year), and add the
+	// snippets slice to it.
+
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
 	// Use the new render helper.
-	app.render(w, r, http.StatusOK, "home.tmpl.html", templateData{
-		Snippets: snippets,
-	})
+	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 
 }
 
@@ -43,10 +47,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 	// Use the new render helper.
-	app.render(w, r, http.StatusOK, "view.tmpl.html", templateData{
-		Snippet: snippet,
-	})
+	app.render(w, r, http.StatusOK, "view.tmpl.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
