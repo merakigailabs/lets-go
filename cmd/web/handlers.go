1f30f80905ad8c20098d9238c2e2137fae21e850
Merakigai <merakigailabs@gmail.com>
2024-12-21 09:21:50 +0700

    ch1: add static file server and static files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,19 +11,12 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
-
 	files := []string{
-		"./ui/html/base.tmpl.html", // /!\ Base Template should be the first file in the slice.
+		"./ui/html/base.tmpl.html",
 		"./ui/html/partials/nav.tmpl.html",
 		"./ui/html/pages/home.tmpl.html",
 	}
 
-	// Use the template.ParseFiles() function to read the files and store the
-	// templates in a template set. Notice that we use ... to pass the contents
-	// of the files slice as variadic arguments.
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
@@ -32,8 +25,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
