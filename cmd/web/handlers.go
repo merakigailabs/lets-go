58c5e2a355aacfa651f3a159fa78ec7b26430c46
Merakigai <merakigailabs@gmail.com>
2024-12-21 19:29:22 +0700

    ch5: remove comments

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,14 +18,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Call the newTemplateData() helper to get a templateData struct containing
-	// the 'default' data (which for now is just the current year), and add the
-	// snippets slice to it.
-
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
 
-	// Use the new render helper.
 	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 
 }
@@ -55,20 +50,15 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	// Create some variables holding dummy data. We'll remove these later on
-	// during the build.
 	title := "O snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := 7
 
-	// Pass the data to the SnippetModel.Insert() method, receiving the
-	// ID of the new record back.
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
