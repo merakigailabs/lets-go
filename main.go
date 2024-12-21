91ca581d6fe7840a94016daa99511ce2cc89026c
Merakigai <merakigailabs@gmail.com>
2024-12-21 08:04:14 +0700

    ch1: Added path value for snippetView fn

--- main.go
+++ main.go
@@ -1,45 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the request body.
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-// Add a snippetView handler function
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet ..."))
+
+	// Extract the value of the id wildcard from the request using r.PathValue()
+	// and try to convert it to an integer using the strconv.Atoi() function. If
+	// it can't be converted to an integer, or the value is less than 1, we
+	// return a 404 page not found response.
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Use the fmt.Sprintf() function to interpolate the id value with a
+	// message, then write it as the HTTP response.
+	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	w.Write([]byte(msg))
 }
 
-// Ads a snippetCreate handler function
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for create a new snippet ..."))
 }
 
 func main() {
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
+
 	mux := http.NewServeMux()
 
-	// Register the two new handler functions and corresponding route patterns with
-	// the servemux, in exactly the same way that we did before.
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// Print a log message to say that the server is starting
 	log.Print("starting server on :4000")
 
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
--- notes.md
+++ notes.md
@@ -5,3 +5,26 @@
  `go run` compiles your code, creates an executable binary in your `/tmp`
 directory.
 
+```go
+
+http.NewServeMux() //  function to initialize a new servemux
+
+```
+
+
+```go
+
+// Add a snippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet ..."))
+}
+
+mux.HandleFunc("/snippet/view", snippetView) // Registers the Handler to  route
+
+```
+
+Servemux sanitize urls
+
+The r.PathValue() method always returns a string value IT IS NOT SANITIZED !
+
+
