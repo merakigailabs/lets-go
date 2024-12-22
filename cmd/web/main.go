32559c9c360e31ef1f034fe3876d75990363e120
Merakigai <merakigailabs@gmail.com>
2024-12-22 16:27:57 +0700

    ch8: adding server error log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -81,6 +81,11 @@ func main() {
 	srv := &http.Server{
 		Addr:    cfg.addr,
 		Handler: app.routes(),
+		// Create a *log.Logger from our structured logger handler, which writes
+		// log entries at Error level, and assign it to the ErrorLog field. If
+		// you would prefer to log the server errors at Warn level instead, you
+		// could pass slog.LevelWarn as the final parameter.
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
 	logger.Info("starting server", "addr", cfg.addr)
