32435cb3fd77ad1ce2c1f71a87d825cca105e6c6
Merakigai <merakigailabs@gmail.com>
2024-12-22 10:12:23 +0700

    ch6: write helper to parse form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -71,22 +71,14 @@ type snippetCreateForm struct {
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	err := r.ParseForm()
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
-	// Declare a new empty instance of the snippetCreateForm struct.
 	var form snippetCreateForm
-	// Call the Decode() method of the form decoder, passing in the current
-	// request and *a pointer* to our snippetCreateForm struct. This will
-	// essentially fill our struct with the relevant values from the HTML form.
-	// If there is a problem, we return a 400 Bad Request response to the client.
-	err = app.formDecoder.Decode(&form, r.PostForm)
+
+	err := app.decodePostForm(r, &form)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
+
 	// Then validate and use the data as normal...
 	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
 	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
