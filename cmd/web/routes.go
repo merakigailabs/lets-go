fad182f3c2edb57a6bf79dc25fac57beefa55c98
Merakigai <merakigailabs@gmail.com>
2024-12-22 08:58:35 +0700

    ch5: added request logger

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,5 +20,8 @@ func (app *application) routes() http.Handler {
 	// Pass rhe servermux as the 'next' parameter to the commonHeaders middleware.
 	// Because commonHeaders is just a function, and the function returns a
 	// http.Handler we don't need to do anything else.
-	return commonHeaders(mux)
+
+	// Wrap the existing chain with the logRequest middleware.
+	return app.logRequest(commonHeaders(mux))
+
 }
